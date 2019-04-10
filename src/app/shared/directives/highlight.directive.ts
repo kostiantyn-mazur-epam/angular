8034a5bc142b770ec5f346b043671903066b56af
Kostiantyn Mazur <Kostiantyn_Mazur@epam.com>
2019-04-10 14:20:32 +0300

    #Task2 - Added ngStyle for buy button.

--- src/app/shared/directives/highlight.directive.ts
+++ src/app/shared/directives/highlight.directive.ts
@@ -1,25 +1,27 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private el: ElementRef) {}
+  private bgColor = null;
+
+  constructor(private el: ElementRef) { }
+
+  @HostBinding('style.backgroundColor') get getBgColor() {
+    return this.bgColor;
+  }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('green');
+    this.bgColor = 'green';
   }
 
   @HostListener('click') onClick() {
-    this.highlight('green');
+    this.bgColor = 'green';
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.highlight(null);
-  }
-
-  private highlight(color: string) {
-    this.el.nativeElement.style.backgroundColor = color;
+    this.bgColor = null;
   }
 }
